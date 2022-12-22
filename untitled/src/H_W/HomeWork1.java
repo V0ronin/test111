f732d403d23fb68d03230017c90afd21cbc70db7
V0ronin <121020027+V0ronin@users.noreply.github.com>
2022-12-22 17:28:25 +0200

    Create HomeWork1.java

---
+++ untitled/src/H_W/HomeWork1.java
@@ -0,0 +1,52 @@
+package H_W;
+
+public class HomeWork1 {
+    public static void main(String[] args) {
+        byte myByte;
+        myByte = 4;
+        byte MyByte;
+        MyByte = 10;
+        System.out.println(myByte);
+        System.out.println(MyByte);
+        short myShort = 1312;
+        short MyShort = 161;
+        System.out.println(myShort);
+        System.out.println(MyShort);
+        int myInt = 34243493;
+        int MyInt = 34243493;
+        System.out.println(myInt);
+        System.out.println(MyInt);
+        long myLong = 4256345534562L;
+        long MyLong = 422909464645362L;
+        System.out.println(myLong);
+        System.out.println(MyLong);
+        float myFloat = 4.56f;
+        float MyFloat = 6.534f;
+        double myDouble =  56.28;
+        double MyDouble =  99.299;
+        boolean isTrue = true;
+        boolean isFalse = false;
+        System.out.println(MyFloat);
+        System.out.println(myFloat);
+        System.out.println(myDouble);
+        System.out.println(MyDouble);
+        System.out.println(isFalse);
+        System.out.println(isFalse);
+        char myChar1 = '\u1312';
+        char myChar2 = 'G';
+        char myChar3 = 21;
+        System.out.println(myChar1);
+        System.out.println(myChar2);
+        System.out.println(myChar3);
+
+
+
+
+
+
+
+
+
+
+    }
+}
