d3084dd522250a768c4a8733aaf3ba02d7ca9ca4
V0ronin <121020027+V0ronin@users.noreply.github.com>
2022-12-22 14:07:06 +0200

    1 com

---
+++ untitled/src/text.java
@@ -0,0 +1,10 @@
+public class text {
+    public static void main(String[] args) {
+        System.out.println("рубаи!");
+        System.out.println("gusi gusi");
+        System.out.println("рубаи!");
+        System.out.println();
+
+    }
+
+}
