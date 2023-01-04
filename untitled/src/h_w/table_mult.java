c3619f2f8a0e0a97fd339a9ed0d9681d8dacded8
V0ronin <121020027+V0ronin@users.noreply.github.com>
2023-01-04 10:25:36 +0200

    upd

---
+++ untitled/src/h_w/table_mult.java
@@ -0,0 +1,14 @@
+package h_w;
+
+public class table_mult {
+    public static void main(String[] args) {
+        for (int i = 1; i <=10 ; i++) {
+            System.out.println();
+            for (int j = 1; j <=10 ; j++) {
+                System.out.print(i * j + " ");
+
+            }
+
+        }
+    }
+}
---
+++ untitled/src/lesson_04/task_1.java
@@ -0,0 +1,14 @@
+package lesson_04;
+
+public class task_1 {
+    public static void main(String[] args) {
+        int count = 13;
+
+        for (int i = count; i >= 1  ; i--) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+
+            }
+        }
+    }
+}
