51a18fcfc6538152044246cf88f056d9c31f2c10
V0ronin <121020027+V0ronin@users.noreply.github.com>
2023-01-09 13:18:50 +0200

    Update DuplicateNumbers.java

--- untitled/src/h_w_5/DuplicateNumbers.java
+++ untitled/src/h_w_5/DuplicateNumbers.java
@@ -1,19 +1,30 @@
 package h_w_5;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Random;
 
 public class DuplicateNumbers {
     public static void main(String[] args) {
-        Random random = new Random();
+        Random rand = new Random();
+        HashMap<Integer, Integer> counter = new HashMap<>();
+
 
         ArrayList<Integer> numbers = new ArrayList<>();
 
         for (int i = 0; i < 100; i++) {
-            numbers.add(random.nextInt(50), +1);
-            System.out.println("numbers = " + numbers);
-
+            numbers.add(rand.nextInt(50));
         }
+
+
+        for (int nomer : numbers) {
+            if (counter.containsKey(nomer)) {
+                counter.put(nomer, (counter.get(nomer) + 1));
+            } else {
+                counter.put(nomer, 1);
+            }
+        }
+        counter.forEach((num, gum) -> System.out.println("we have num " + num + ", " + gum + " times"));
 
 
     }
